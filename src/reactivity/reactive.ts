3e1d02eec19ccf60976250aac81fc1733478ce42
Song0129 <Song_3191@163.com>
2022-03-09 15:15:23 +0800

    update

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,6 +1,9 @@
 import { track, trigger } from "./effect";
 
 export function reactive(raw) {
+	// 返回proxy对象，代理整个对象raw
+	// get时，收集依赖即收集内容改变时的回调函数fn
+	// set时，触发依赖即触发内容改变时需执行的函数fn
 	return new Proxy(raw, {
 		get(target, key) {
 			// {foo:1}
