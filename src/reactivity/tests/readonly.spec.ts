84c7ea049b38aa3de64244aa1bbe5e9cee20bd0f
Song0129 <Song_3191@163.com>
2022-03-31 16:22:33 +0800

    feat: 实现readonly和reactive嵌套对象转换功能

--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -1,19 +1,23 @@
 import { isReadonly, readonly } from "../reactive";
 
 describe("readonly", () => {
-	it("happy path", () => {
+	it("should make nested values readonly", () => {
 		// not set
 		const original = { foo: 1, bar: { baz: 2 } };
 		const wrapped = readonly(original);
 
+		// 实现readonly
 		expect(isReadonly(wrapped)).toBe(true);
 		expect(isReadonly(original)).toBe(false);
+
+		expect(isReadonly(wrapped.bar)).toBe(true);
+		expect(isReadonly(original.bar)).toBe(false);
 
 		expect(wrapped).not.toBe(original);
 		expect(wrapped.foo).toBe(1);
 	});
 
-	it("warn then call set", () => {
+	it("should call console.warn then call set", () => {
 		// console.warn()
 		// mock
 		console.warn = jest.fn();
