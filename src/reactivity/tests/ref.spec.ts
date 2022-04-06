2403b1bfc911007c0eb884b788914ed4340247ee
Song0129 <Song_3191@163.com>
2022-04-06 17:04:54 +0800

    feat: 实现ref功能

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -2,7 +2,7 @@ import { effect } from "../effect";
 import { ref } from "../ref";
 
 describe("ref", () => {
-	it.only("happy path", () => {
+	it("happy path", () => {
 		const a = ref(1);
 		expect(a.value).toBe(1);
 	});
@@ -25,6 +25,19 @@ describe("ref", () => {
 		// same value should not trigger
 		a.value = 2;
 		expect(calls).toBe(2);
+		expect(dummy).toBe(2);
+	});
+
+	it.skip("should make nested properties reactive", () => {
+		const a = ref({
+			count: 1,
+		});
+		let dummy;
+		effect(() => {
+			dummy = a.value.count;
+		});
+		expect(dummy).toBe(1);
+		a.value.count = 2;
 		expect(dummy).toBe(2);
 	});
 });
