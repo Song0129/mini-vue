e53b0eb7d4df0c7ce08628fa64673aef35521a97
Song0129 <Song_3191@163.com>
2022-04-07 16:00:27 +0800

    feat: 实现ref功能

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,11 +1,17 @@
-import { hasChanges } from "../shared";
+import { hasChanges, isObject } from "../shared";
 import { isTracking, trackEffects, triggerEffects } from "./effect";
+import { reactive } from "./reactive";
 
 class RefImpl {
 	private _value: any;
+	private _rawValue: any;
 	public dep;
 	constructor(value) {
-		this._value = value;
+		this._rawValue = value;
+		this._value = convert(value);
+		// value -> this._value
+		// 1. 看看value 是不是对象
+
 		this.dep = new Set();
 	}
 	get value() {
@@ -16,11 +22,17 @@ class RefImpl {
 		// 一定先去修改了value值
 		// newValue -> this._value
 		// hasChanged
-		if (hasChanges(newValue, this._value)) {
-			this._value = newValue;
+		// 对比的时候 object
+		if (hasChanges(newValue, this._rawValue)) {
+			this._rawValue = newValue;
+			this._value = convert(newValue);
 			triggerEffects(this.dep);
 		}
 	}
+}
+
+function convert(value) {
+	return isObject(value) ? reactive(value) : value;
 }
 
 function trackRefValue(ref) {
