fb7f7ef964652b8952edaa09eea15df0243ec0a8
Song0129 <Song_3191@163.com>
2022-04-18 18:21:55 +0800

    feat: 实现computed功能

--- src/reactivity/computed.ts
+++ src/reactivity/computed.ts
@@ -1,34 +1,34 @@
 import { ReactiveEffect } from "./effect";
 
-class ComputedRefImpl{
+class ComputedRefImpl {
   private _getter: any;
-  private _dirty: boolean=true;
+  private _dirty: boolean = true;
   private _value: any;
   private _effect: ReactiveEffect;
 
-  constructor(getter){
+  constructor(getter) {
     this._getter = getter;
 
-    this._effect = new ReactiveEffect(getter,()=>{
+    this._effect = new ReactiveEffect(getter, () => {
       if (!this._dirty) {
-      this._dirty = true;
+        this._dirty = true;
       }
     });
   }
 
-  get value(){
+  get value() {
     // get 
     // get value -> dirty true
     // 当依赖的响应式对象的值发生变化的时候
     // effect
-    if(this._dirty){
+    if (this._dirty) {
       this._dirty = false;
       this._value = this._effect.run();
-    }    
+    }
     return this._value;
   }
 }
 
-export function computed(getter){
-    return new ComputedRefImpl(getter);
+export function computed(getter) {
+  return new ComputedRefImpl(getter);
 }
--- src/reactivity/tests/computed.spec.ts
+++ src/reactivity/tests/computed.spec.ts
@@ -7,18 +7,18 @@ describe("computed", () => {
     // ref 
     // .value
     // 1.缓存
-    const user = reactive({age:1});
+    const user = reactive({ age: 1 });
 
     const age = computed(() => user.age);
-    
+
     expect(age.value).toBe(1);
   })
 
-  it("should compute lazily",()=>{
+  it("should compute lazily", () => {
     const value = reactive({
-      foo:1,
+      foo: 1,
     });
-    const getter = jest.fn(()=>{
+    const getter = jest.fn(() => {
       return value.foo;
     })
 
@@ -38,12 +38,12 @@ describe("computed", () => {
     value.foo = 2;// trigger -> effect ->get 重新执行了
     expect(getter).toHaveBeenCalledTimes(1);
 
-    // // now it should compute
-    // expect(cValue.value).toBe(2);
-    // expect(getter).toHaveBeenCalledTimes(2);
+    // now it should compute
+    expect(cValue.value).toBe(2);
+    expect(getter).toHaveBeenCalledTimes(2);
 
-    // // should not compute again
-    // cValue.value;
-    // expect(getter).toHaveBeenCalledTimes(2);
+    // should not compute again
+    cValue.value;
+    expect(getter).toHaveBeenCalledTimes(2);
   })
 })
