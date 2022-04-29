861a77b6e93f4a5ea9a3f6111d0ba37af31fb8dd
Song0129 <Song_3191@163.com>
2022-04-29 16:39:15 +0800

    feta: 实现组件代理对象

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -1,3 +1,5 @@
+import { PublicInstanceProxyHandlers } from "./componentPublicInstance";
+
 export function createComponentInstance(vnode) {
 	const component = {
 		vnode,
@@ -18,23 +20,7 @@ function setupStatefulComponent(instance: any) {
 	const Component = instance.type;
 
 	// ctx
-	instance.proxy = new Proxy(
-		{},
-		{
-			get(target, key) {
-				// setupState
-				const { setupState } = instance;
-				if (key in setupState) {
-					return setupState[key];
-				}
-
-				// key -> $el
-				if (key === "$el") {
-					return instance.vnode.el;
-				}
-			},
-		}
-	);
+	instance.proxy = new Proxy({ _: instance }, PublicInstanceProxyHandlers);
 
 	const { setup } = Component;
 	if (setup) {
