dee621b29b406f48a409976431ddad48232b7b7d
Only0129 <Song_3191@163.com>
2022-05-19 18:19:47 +0800

    feat: 实现组件emit功能

--- src/runtime-core/componentEmit.ts
+++ src/runtime-core/componentEmit.ts
@@ -1,3 +1,5 @@
+import { camelize, toHandlerKey } from "../shared/index";
+
 export function emit(instance, event, ...args) {
 	console.log("emit", event);
 
@@ -8,19 +10,6 @@ export function emit(instance, event, ...args) {
 	// 先写一个特定的行为 -》 重构成通用的行为
 	// add->Add
 	// add-foo -> addFoo
-	const capitalize = (str: string) => {
-		return str.charAt(0).toUpperCase() + str.slice(1);
-	};
-
-	const camelize = (str: string) => {
-		return str.replace(/-(\w)/g, (_, c: string) => {
-			return c ? c.toUpperCase() : "";
-		});
-	};
-
-	const toHandlerKey = (str: string) => {
-		return str ? "on" + capitalize(str) : "";
-	};
 
 	const handlerName = toHandlerKey(camelize(event));
 	const handler = props[handlerName];
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -7,3 +7,17 @@ export const hasChanges = (val, newVlaue) => {
 };
 
 export const hasOwn = (val, key) => Object.prototype.hasOwnProperty.call(val, key);
+
+export const capitalize = (str: string) => {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const camelize = (str: string) => {
+	return str.replace(/-(\w)/g, (_, c: string) => {
+		return c ? c.toUpperCase() : "";
+	});
+};
+
+export const toHandlerKey = (str: string) => {
+	return str ? "on" + capitalize(str) : "";
+};
