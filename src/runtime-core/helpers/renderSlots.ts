1ae8f483d232eecf366c57a9a6476ac58ed19c56
Only0129 <Song_3191@163.com>
2022-05-23 20:59:35 +0800

    feat: 实现组件slots功能

--- src/runtime-core/helpers/renderSlots.ts
+++ src/runtime-core/helpers/renderSlots.ts
@@ -5,6 +5,7 @@ export function renderSlots(slots, name, props) {
 	const slot = slots[name];
 	if (slot) {
 		if (typeof slot === "function") {
+			// children 是不可以有 array
 			return createVNode("div", {}, slot(props));
 		}
 	}
