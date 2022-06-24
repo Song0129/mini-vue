b0beca9aec99dd21abb2c68031c6c46442647d87
Song0129 <Song_3191@163.com>
2022-06-24 14:59:49 +0800

    feat: 实现解析element标签

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast";
 
+const enum TagType {
+	Start,
+	End,
+}
+
 export function baseParse(content: string) {
 	const context = createParseContext(content);
 
@@ -11,18 +16,48 @@ function parseChildren(context) {
 
 	// 满足插值表达式  {{}}
 	let node;
-	if (context.source.startsWith("{{")) {
+	const s = context.source;
+
+	if (s.startsWith("{{")) {
 		node = parseInterPolation(context);
+	} else if (s[0] === "<") {
+		if (/[a-z]/i.test(s[1])) {
+			node = parseElement(context);
+		}
 	}
 	nodes.push(node);
 
 	return nodes;
 }
 
+function parseElement(context) {
+	// Implement
+
+	const element = parseTag(context, TagType.Start);
+	parseTag(context, TagType.End);
+
+	return element;
+}
+
+function parseTag(context: any, type: TagType) {
+	// 1. 解析tag
+	const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+
+	const tag = match[1];
+	// 2. 删除处理完成的代码
+	advanceBy(context, match[0].length);
+	advanceBy(context, 1);
+
+	if (type === TagType.End) return;
+
+	return { type: NodeTypes.ELEMENT, tag };
+}
+
 function parseInterPolation(context) {
 	// {{message}}
 	const openDelimiter = "{{";
 	const clostDelimiter = "}}";
+
 	const closeIndex = context.source.indexOf(clostDelimiter, openDelimiter.length);
 
 	advanceBy(context, openDelimiter.length);
@@ -33,7 +68,6 @@ function parseInterPolation(context) {
 
 	advanceBy(context, rawContentLength + clostDelimiter.length);
 
-	console.log(context.source);
 	return {
 		type: NodeTypes.INTERPOLATION,
 		content: {
